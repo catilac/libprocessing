f967a3edd1dcfd715b40642bcaee0057d524fd8a
Moon <catilac@users.noreply.github.com>
2025-12-31 14:34:53 -0800

    [processing_pyo3] Image class (#54)
    Co-authored-by: charlotte 🌸 <charlotte.c.mcelwain@gmail.com>

--- crates/processing_pyo3/examples/background_image.py
+++ crates/processing_pyo3/examples/background_image.py
@@ -1,11 +1,14 @@
 from processing import *
 
 def setup():
+    global i
     size(800, 600)
+    i = image("images/logo.png")
+
 
 def draw():
-    background(220)
-    image("images/logo.png")
+    background(220, 100, 24)
+    background(i)
 
 
 # TODO: this should happen implicitly on module load somehow
--- crates/processing_pyo3/src/graphics.rs
+++ crates/processing_pyo3/src/graphics.rs
@@ -23,6 +23,18 @@ impl Drop for Surface {
     }
 }
 
+#[pyclass]
+#[derive(Debug)]
+pub struct Image {
+    entity: Entity,
+}
+
+impl Drop for Image {
+    fn drop(&mut self) {
+        let _ = image_destroy(self.entity);
+    }
+}
+
 #[pyclass(unsendable)]
 pub struct Graphics {
     entity: Entity,
@@ -68,6 +80,11 @@ impl Graphics {
         let (r, g, b, a) = parse_color(&args)?;
         let color = bevy::color::Color::srgba(r, g, b, a);
         graphics_record_command(self.entity, DrawCommand::BackgroundColor(color))
+            .map_err(|e| PyRuntimeError::new_err(format!("{e}")))
+    }
+
+    pub fn background_image(&self, image: &Image) -> PyResult<()> {
+        graphics_record_command(self.entity, DrawCommand::BackgroundImage(image.entity))
             .map_err(|e| PyRuntimeError::new_err(format!("{e}")))
     }
 
@@ -124,10 +141,11 @@ impl Graphics {
         .map_err(|e| PyRuntimeError::new_err(format!("{e}")))
     }
 
-    pub fn image(&self, file: &str) -> PyResult<()> {
-        let image = image_load(file).unwrap();
-        graphics_record_command(self.entity, DrawCommand::BackgroundImage(image))
-            .map_err(|e| PyRuntimeError::new_err(format!("{e}")))
+    pub fn image(&self, file: &str) -> PyResult<Image> {
+        match image_load(file) {
+            Ok(image) => Ok(Image { entity: image }),
+            Err(e) => Err(PyRuntimeError::new_err(format!("{e}"))),
+        }
     }
 
     pub fn push_matrix(&self) -> PyResult<()> {
